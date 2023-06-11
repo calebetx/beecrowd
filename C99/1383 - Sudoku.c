979838f50c6862dd9cc218f3492933d17c8152ca
Gabriel Calebe <105611134+GabrielCGSc@users.noreply.github.com>
2023-06-11 17:21:05 -0300

    a

--- C99/1383 - Sudoku.c
+++ C99/1383 - Sudoku.c
@@ -1,8 +1,40 @@
 #include <stdio.h>
 
+int verLC(struct local posicao, int jogoTodo[8][8]){
+    int x = posicao.i;
+    int y = posicao.j;
+
+    // Linha
+    for (size_t i = 0; i < 9; i++)
+    {
+        if(jogoTodo[x][y] == jogoTodo[x][i]){
+            if(y != i)
+                return 0;
+        }
+    }
+
+    // Coluna
+    for (size_t i = 0; i < 9; i++)
+    {
+        if(jogoTodo[x][y] == jogoTodo[i][y]){
+            if(x != i)
+                return 0;
+        }
+    }
+
+    return 1;
+}
+
+struct local{
+    int i;
+    int j;
+};
+
+
 int main(){
 
     int jogoTodo[8][8];
+    int posicao;
     int n;
 
     scanf("%d", &n);
@@ -17,7 +49,17 @@ int main(){
                 }
             }
 
-            // Bloco
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    struct local posicao;
+                    posicao.i = i;
+                    posicao.j = j;
+
+                    printf("%d", verLC(posicao, jogoTodo[8][8]));
+                }
+            }
             
         }
 
